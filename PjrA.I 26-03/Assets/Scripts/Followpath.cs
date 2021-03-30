d8a58fe45665c29963065b453f3e43a4f22513e2
unknown <50969791+KatsumiCaio@users.noreply.github.com>
2021-03-29 21:37:10 -0300

    IAV1
    Primeira parte com os códigos e WayPoints

--- PjrA.I 26-03/Assets/Scripts/Followpath.cs
+++ PjrA.I 26-03/Assets/Scripts/Followpath.cs
@@ -36,6 +36,28 @@ public class Followpath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength()) ;
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        //O nó que estará mais próximo neste momento
+        currentNode = g.getPathPoint(currentWP);
+
+        //se estivermos mais próximo bastante do nó o tanque se moverá para o próximo
+        if(Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        if(currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction),
+                Time.deltaTime * rotSpeed);
+        }
+            
     }
 }
