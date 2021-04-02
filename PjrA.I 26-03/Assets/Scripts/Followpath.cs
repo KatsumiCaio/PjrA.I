d12633694c3aa8746aad13a8db994e4c61bc795d
unknown <50969791+KatsumiCaio@users.noreply.github.com>
2021-04-02 20:31:09 -0300

    EX waypoint
    Comentando os códigos

--- PjrA.I 26-03/Assets/Scripts/Followpath.cs
+++ PjrA.I 26-03/Assets/Scripts/Followpath.cs
@@ -4,59 +4,59 @@ using UnityEngine;
 
 public class Followpath : MonoBehaviour
 {
-    Transform goal;
-    float speed = 5.0f;
-    float accuracy = 1.0f;
-    float rotSpeed = 2.0f;
+    Transform goal; // para identifcar a variavel que será colocada como final. 
+    float speed = 5.0f;// variavel que representa a velocidade de movimentação do obejeto.
+    float accuracy = 1.0f;// é usado para que quando estiver se aproximando já começar a proxima parte.
+    float rotSpeed = 2.0f;// a velocidade de rotação do objeto.
 
-    public GameObject wpManager;
-    GameObject[] wps;
-    GameObject currentNode;
+    public GameObject wpManager; // referencia ao WpManager.
+    GameObject[] wps;// pegar a lista de informação wps.
+    GameObject currentNode;// pegando os Nodes informados.
     int currentWP = 0;
-    Graph g;
+    Graph g; // falando que Graph sera representado como G
     void Start()
     {
-        wps = wpManager.GetComponent<WpManager>().waypoints;
-        g = wpManager.GetComponent<WpManager>().graph;
-        currentNode = wps[0];
+        wps = wpManager.GetComponent<WpManager>().waypoints;// vai pegar os waypoints do WpManager
+        g = wpManager.GetComponent<WpManager>().graph;// vai pegar os graphs do WpManager
+        currentNode = wps[0]; // iniciando o wps como 0
     }
 
-    public void GoToHeli()
+    public void GoToHeli() // vai fazer o tank se mover para o Heliporto 
     {
-        g.AStar(currentNode, wps[1]);
-        currentWP = 0;
+        g.AStar(currentNode, wps[1]); // indica onde esta o ponto que leva para o Heliporto
+        currentWP = 0; 
     }
 
-    public void GoToRuin()
+    public void GoToRuin()// vai fazer o tank se mover para as ruinas
     {
-        g.AStar(currentNode, wps[6]);
+        g.AStar(currentNode, wps[6]); //indica onde esta o ponto que leva para as ruinas
         currentWP = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength()) // se o g for = 0 ou 
             return;
 
         //O nó que estará mais próximo neste momento
         currentNode = g.getPathPoint(currentWP);
 
         //se estivermos mais próximo bastante do nó o tanque se moverá para o próximo
-        if(Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        if(Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy) // adicionando que quando chegar a uma determinada distancia vai fazer o accuracy para poder continuar e realizar outro objetivo.
         {
             currentWP++;
         }
 
-        if(currentWP < g.getPathLength())
+        if(currentWP < g.getPathLength()) 
         {
             goal = g.getPathPoint(currentWP).transform;
             Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
 
-            Vector3 direction = lookAtGoal - this.transform.position;
+            Vector3 direction = lookAtGoal - this.transform.position; 
 
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction),
-                Time.deltaTime * rotSpeed);
+                Time.deltaTime * rotSpeed); // ele vai se movimentar para o outro ponto de forma mais contornada.
         }
             
     }
--- PjrA.I 26-03/Assets/Scripts/WpManager.cs
+++ PjrA.I 26-03/Assets/Scripts/WpManager.cs
@@ -6,29 +6,29 @@ using UnityEngine;
 
 public struct Link
 {
-    public enum direction { UNI, BI}
-    public GameObject node1;
-    public GameObject node2;
-    public direction dir;
+    public enum direction { UNI, BI} //variavel que irá definir se a movimentação será só ida ou de ida e volta.
+    public GameObject node1; //variavel de Objeto.
+    public GameObject node2; // variavel de Objeto.
+    public direction dir; // Variavel que irá servir para direcionar o tank.
 }
 public class WpManager : MonoBehaviour
 {
-    public GameObject[] waypoints;
-    public Link[] links;
-    public Graph graph = new Graph();
+    public GameObject[] waypoints; //lista para os Waypoints.
+    public Link[] links; // Lista para os Links que serão usados para decidir a forma da movimentação do tank.
+    public Graph graph = new Graph(); // para mostrar o caminho que será traçado.
 
     void Start()
     {
-        if(waypoints.Length> 0)
+        if(waypoints.Length> 0) // vê o tamanho do waypoint e tem que ser maior que 0
         {
-            foreach(GameObject wp in waypoints)
+            foreach(GameObject wp in waypoints) // foreach com wp e o armazenamento dos waypoints
             {
-                graph.AddNode(wp);
+                graph.AddNode(wp); // adiciona um node dentro do grafo 
             }
-            foreach(Link l in links)
+            foreach(Link l in links) 
             {
-                graph.AddEdge(l.node1, l.node2);
-                if (l.dir == Link.direction.BI) graph.AddEdge(l.node2, l.node1);
+                graph.AddEdge(l.node1, l.node2); // adicionando aresta  
+                if (l.dir == Link.direction.BI) graph.AddEdge(l.node2, l.node1); // condional da aresta falando que quando for BI vai ligar um grafo do nó 2 a um nó 1 
             }
         }
     }
@@ -36,6 +36,6 @@ public class WpManager : MonoBehaviour
     
     void Update()
     {
-        graph.debugDraw();
+        graph.debugDraw(); // Vai desenhar o caminho.
     }
 }
